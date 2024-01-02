8532f1fc9763112324cf4bc7c7e9177835b9a71b
Amangoua Ange <adagou@yahoo.com>
2024-01-02 11:50:27 +0000

    creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+
+export const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/opportunities" element={<Opportunities />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/solutions" element={<Solutions />} />
+    </Routes>
+  </Router>
+);
+
+export function Opportunities() {
   return (
     <>
-      <div>
-        <h2 className="text-5xl">Hello React</h2>
-      </div>
+      <div>Opportunities</div>
     </>
   );
 }
 
-export default App;
+export function Contact() {
+  return (
+    <>
+      <div>Contact</div>
+    </>
+  );
+}
+
+export function Solutions() {
+  return (
+    <>
+      <div>Solutions</div>
+    </>
+  );
+}
